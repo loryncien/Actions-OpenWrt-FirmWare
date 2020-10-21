3dc6650d115a9bfc6bd7b9df4f4d1406e7814de6
loryncien <lluocheng88@gmail.com>
2020-10-21 15:16:09 +0800

    Add files via upload

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,8 +10,44 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# Uncomment a feed source and Add a feed source
+# 使用源码自带ShadowSocksR Plus+出国软件
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-package;master' feeds.conf.default
+sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#添加自定义插件链接
+git clone https://github.com/Lienol/openwrt-package package/lienol-packages
+git clone https://github.com/siropboy/sirpdboy-package package/sirpdboy-package
+#git clone https://github.com/kenzok8/openwrt-packages package/kenzok8-package
+
+#添加自定义插件
+echo '下载 openclash'
+git clone -b master https://github.com/vernesong/OpenClash.git package/openwrt-packages/luci-app-openclash
+
+#echo '下载 luci-app-sqm'
+#svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-app-sqm package/openwrt-packages/luci-app-sqm
+#svn co https://github.com/openwrt/luci/trunk/applications/luci-app-sqm package/openwrt-packages/luci-app-sqm
+#echo '下载 sqm-scripts'
+#svn co https://github.com/siropboy/sirpdboy-package/trunk/sqm-scripts package/openwrt-packages/sqm-scripts
+svn co https://github.com/openwrt/packages/trunk/net/sqm-scripts package/openwrt-packages/sqm-scripts
+rm -rf package/sirpdboy-package/sqm-scripts
+sed -i 's/..\/..\/luci.mk/\$\(TOPDIR\)\/feeds\/luci\/luci.mk/g' package/sirpdboy-package/luci-app-nlbwmon/Makefile
+
+echo '下载 ServerChan'
+git clone https://github.com/tty228/luci-app-serverchan.git package/openwrt-packages/luci-app-serverchan
+#echo '下载 kpr+'
+#svn co https://github.com/siropboy/mypackages/trunk/luci-app-koolproxyR package/openwrt-packages/luci-app-koolproxyR
+#git clone https://github.com/cnzd/luci-app-koolproxyR.git package/openwrt-packages/luci-app-koolproxyR
+echo '下载 Dockerman'
+git clone https://github.com/KFERMercer/luci-app-dockerman  package/openwrt-packages/luci-app-dockerman
+echo '主题 infinityfreedom'
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/openwrt-packages/luci-theme-infinityfreedom
+echo '主题 opentomato'
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-opentomato package/openwrt-packages/luci-theme-opentomato
+
+#echo '下载koolddns'
+#git clone https://github.com/bin20088/luci-app-koolddns.git package/openwrt-packages/luci-app-koolddns
+#赋予koolddns权限
+#chmod 0755 package/openwrt-packages/luci-app-koolddns/root/etc/init.d/koolddns
+#chmod 0755 package/openwrt-packages/luci-app-koolddns/root/usr/share/koolddns/aliddns
--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,35 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+#清理插件
+rm -rf package/lean/luci-theme-argon
+
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 文件管理中文乱码问题
+sed -i 's/IOCHARSET="iso8859-1"/IOCHARSET="utf8"/g' target/linux/generic/config-5.4
+# V2ray 4.27.5版本
+#sed -i 's/VERSION:=.*$/VERSION:=4.27.5/g' package/lienol-packages/package/v2ray/Makefile
+#sed -i 's/HASH:=.*$/HASH:=f289d8d85ab0851851a6e3c101226e77bed0052fd60f9185df8852b601e657f8/g' package/lienol-packages/package/v2ray/Makefile
+#sed -i 's/VERSION:=.*$/VERSION:=1.4.2/g' package/lienol-packages/package/v2ray-plugin/Makefile
+#sed -i 's/HASH:=.*$/HASH:=4da541c2a6548be24db68ed74ee117b757b6620a9b61b5960637a71f77ad438a/g' package/lienol-packages/package/v2ray-plugin/Makefile
+#sed -i 's/VERSION:=.*$/VERSION:=4.27.5/g' package/lean/v2ray/Makefile
+#sed -i 's/HASH:=.*$/HASH:=f289d8d85ab0851851a6e3c101226e77bed0052fd60f9185df8852b601e657f8/g' package/lean/v2ray/Makefile
+#sed -i 's/VERSION:=.*$/VERSION:=1.4.2/g' package/lean/v2ray-plugin/Makefile
+#sed -i 's/HASH:=.*$/HASH:=4da541c2a6548be24db68ed74ee117b757b6620a9b61b5960637a71f77ad438a/g' package/lean/v2ray-plugin/Makefile
+
+# 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
+#sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-123'' package/lean/default-settings/files/zzz-default-settings
+#echo "DISTRIB_REVISION='S$(date +%Y.%m.%d) Ryan'" > ./package/base-files/files/etc/openwrt_release1
+# 版本号里显示一个自己的名字（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
+#sed -i "s/OpenWrt /281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+
+# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+
+# 修改内核版本
+sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' target/linux/x86/Makefile
+sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' target/linux/x86/Makefile
